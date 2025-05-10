0f0640dee15457a768715aed99ac78149b2332e9
Arnavghimire <ghimirearnav738@gmail.com>
2025-05-10 23:42:37 +0545

    add it

---
+++ que1.py
@@ -0,0 +1,12 @@
+def midpoint(a,b):
+  return a-b
+ten=midpoint(1.606865,1.629048)
+print("10 =",ten)
+thirty=midpoint(1.626645,1.629048)
+print("30 =",thirty)
+fifty=midpoint(1.628185,1.629048)
+print("50 =",fifty)
+hundred=midpoint(1.628832,1.629048)
+print("100 =",hundred)
+fivehundred=midpoint(1.629040,1.629048)
+print("500 =",fivehundred)
